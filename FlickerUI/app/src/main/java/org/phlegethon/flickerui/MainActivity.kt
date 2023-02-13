4c9df732415c49091ed08efb4be37f84bf3cc4b7
Tim Deegan <tjd@phlegethon.org>
2023-02-13 12:54:20 +0000

    UI: default Android app

---
+++ FlickerUI/app/src/main/java/org/phlegethon/flickerui/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.phlegethon.flickerui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.phlegethon.flickerui.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
